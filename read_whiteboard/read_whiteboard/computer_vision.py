5e05d69bafe550c4738939cce73a4423a43188f9
blackm159 <meganblack2027@u.northwestern.edu>
2023-11-09 15:31:27 -0600

    Change to use realsense2 node for getting frames

--- read_whiteboard/read_whiteboard/computer_vision.py
+++ read_whiteboard/read_whiteboard/computer_vision.py
@@ -24,6 +24,7 @@ from std_msgs.msg import String
 from rcl_interfaces.msg import ParameterDescriptor
 from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import Marker, MarkerArray
+from sensor_msgs.msg import Image
 
 # Other libraries
 import math
@@ -53,62 +54,42 @@ class ComputerVision(Node):
         # define parameters
         self.dt = 1/100.0  # 100 Hz
 
+        self.frame = Image().data
+
+        # Subscribers
+        self.get_image = self.create_subscription(Image, "/camera/color/image_raw", self.get_image_callback, QoSProfile(depth=10))
+
         # Services
-        self.get_image = self.create_service(String, "get_image", self.get_image_callback)
+        self.get_string = self.create_service(String, "get_string", self.get_string_callback)
 
         # Timer
         self.tmr = self.create_timer(self.dt, self.timer_callback)
 
-        self.setup_camera()
+        # self.setup_camera()
 
     def timer_callback(self):
         """Control the camera and computer vision."""
         pass
 
-    def get_image_callback(self, request, response):
-        """Return an image."""
+    def get_image_callback(self, msg):
+        """Get an image from the camera."""
+        self.frame = msg.data
 
-        frameset = self.pipeline.wait_for_frames()
+    def get_string_callback(self, request, response):
+        """Find characters in image and return string."""
 
-        color_frame = frameset.get_color_frame()
-        color_image = np.asanyarray(color_frame.get_data())
+        cv2.imwrite("filename3.png", self.frame)
 
-        cv2.imwrite("filename.png", color_image)
+        # reader = easyocr.Reader(['es', 'en', 'de', 'fr'], gpu=False, detail=0)  # this needs to run only once to load the model into memory
+        # results = reader.readtext('filename3.png')
 
-        reader = easyocr.Reader(['es', 'en', 'de', 'fr'], gpu=False, detail=0)  # this needs to run only once to load the model into memory
-        results = reader.readtext('filename.png')
-
-        for result in results:
-            self.get_logger().info(f"Text: {result}")
+        # for result in results:
+        #     self.get_logger().info(f"Text: {result}")
 
         response = String()
         response.data = "Hello World"
 
         return response
-
-    def setup_camera(self):
-        # Create a pipeline
-        self.pipeline = rs.pipeline()
-
-        # Create a config and configure the pipeline to stream
-        #  different resolutions of color and depth streams
-        self.config = rs.config()
-
-        # Get device product line for setting a supporting resolution
-        self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
-        self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
-        self.device = self.pipeline_profile.get_device()
-        self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
-
-        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-        if self.device_product_line == 'L500':
-            self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-        else:
-            self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-        # Start streaming
-        self.profile = self.pipeline.start(self.config)
 
 
 def entry_point(args=None):
