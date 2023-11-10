81d82eaa4604e04e1f2f1d4a5d160fbb0c6e712f
Henry Buron <henryburon34@gmail.com>
2023-11-10 01:55:40 -0600

    Added orientation collection

--- apriltags/apriltags/get_apriltags.py
+++ apriltags/apriltags/get_apriltags.py
@@ -17,33 +17,25 @@ class GetAprilTags(Node):
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
 
-
-
-
-
     def timer_callback(self):
 
+        # Needs to be able to get data from multiple AprilTags, not just this one
         try:
             t = self.tf_buffer.lookup_transform(
                 "tag36h11:9",
-                "camera_link",
+                "camera_link", # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
                 rclpy.time.Time())
         except TransformException as ex:
             self.get_logger().info(
-                        f'Could not transform {"tag36h11:9"} to {"camera_link"}: {ex}') # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
+                        f'Could not transform {"tag36h11:9"} to {"camera_link"}: {ex}')
             return
         
-        self.get_logger().info(f"{t.transform.translation.z}")
+        pose_info = f"Position: {t.transform.translation.x, t.transform.translation.y, t.transform.translation.z}\n"\
+                    f"Orientation: {t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w}"
 
-        # camera_apriltag1 = TransformStamped()
-
-        
-
+        self.get_logger().info("\n" + pose_info + "\n")
 
         
-
-
-
 def get_apriltags_entry(args=None):
     rclpy.init(args=args)
     node = GetAprilTags()
