13afa3b4fa1bdd7661fcefba665aae26b9ba4698
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-11-09 18:48:00 -0600

    succesfully built and run a test publisher node that sends hardcoded messages in diff languages, this is to test to see how ROS msgs respond with diff languages, esp non-latin ones e.g. chinese; and it works well, the messages are received

--- translation_pkg/translation_pkg/translator_node.py
+++ translation_pkg/translation_pkg/translator_node.py
@@ -1,6 +1,61 @@
-def main():
-    print('Hi from translation_pkg.')
+"""
+Test publisher node to see how non-latin characters (e.g. Chinese) fair in msgs with ROS
+
+"""
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
 
-if __name__ == '__main__':
-    main()
+class Translator(Node):
+    """WIP"""
+
+    def __init__(self):
+        # Initialize the node
+        super().__init__("translator")
+        # Initialize variables
+        self.init_var()
+
+        #
+        # PUBLISHERS
+        #
+        self.pub_string = self.create_publisher(String, "message", 10)
+
+        #
+        # TIMER
+        #
+        period = 1 / self.frequency
+        self.timer = self.create_timer(period, self.timer_callback)
+
+    def init_var(self):
+        """WIP"""
+        self.frequency = 1
+
+    #
+    # TIMER CALLBACK
+    #
+    def timer_callback(self):
+        """WIP"""
+        english_msg = String()
+        english_msg.data = "English: Hello my name is Damien"
+        self.pub_string.publish(english_msg)
+
+        chinese_msg = String()
+        chinese_msg.data = "Chinese: 你好，我的名字是 Damien"
+        self.pub_string.publish(chinese_msg)
+
+        french_msg = String()
+        french_msg.data = "French: Bonjour, je m'appelle Damien"
+        self.pub_string.publish(french_msg)
+
+        hindi_msg = String()
+        hindi_msg.data = "Hindi: हैलो मेरा नाम है Damien"
+        self.pub_string.publish(hindi_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Translator()
+    rclpy.spin(node)
+    rclpy.shutdown()
