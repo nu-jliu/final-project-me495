2817f6f513e7d74c03940dd9fab9ee9cd41c07f0
dkoh555 <damienkoh2025@u.northwestern.edu>
2023-11-09 18:05:11 -0600

    created translation package

---
+++ translation_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'translation_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='damien',
+    maintainer_email='damienkoh2025@u.northwestern.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'translator_node = translation_pkg.translator_node:main'
+        ],
+    },
+)
