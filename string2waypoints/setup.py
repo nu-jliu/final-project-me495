ec01394711f7ce02465a5efc6dcb1e29771c4814
kassidyshedd <kass92800@gmail.com>
2023-11-14 08:37:11 -0600

    Created string2waypoints package

---
+++ string2waypoints/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'string2waypoints'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Kassidy Shedd',
+    maintainer_email='kass92800@gmail.com',
+    description='This node takes a string as an input and will output corresponding waypoints allowing the robot to write the string.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
