29d5c246fc18bc6cc576f4a2730dbfa73432c07b
Henry Buron <henryburon34@gmail.com>
2023-11-14 19:08:45 -0600

    Distance is working. Removed everything about franka

--- apriltags/apriltags/get_apriltags.py
+++ apriltags/apriltags/get_apriltags.py
@@ -22,7 +22,7 @@ class GetAprilTags(Node):
     def __init__(self):
         super().__init__("get_apriltags")
         
-        self.state = State.WAITING
+        self.state = State.LOOK_UP_TRANSFORM
 
         self.timer = self.create_timer(1.0/100.0, self.timer_callback)
         
@@ -30,78 +30,41 @@ class GetAprilTags(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.tf_broadcaster = TransformBroadcaster(self)
-        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
 
-        self.broadcast_static_transform()
-        
-        # Initialize robot information
-        self.robot = MoveRobot(self, "panda_manipulator", True, "panda_hand_tcp")
-        self.orientation: Quaternion = None
 
-        self.whiteboard_distance = None
-
-        self.top_left = None
-        self.bottom_left = None
-        self.top_right = None
 
 #########################################################################################################################
     def timer_callback(self):
 
         if self.state == State.LOOK_UP_TRANSFORM:
 
-            ########## TOP LEFT
             try:
                 t = self.tf_buffer.lookup_transform(
-                    "tag36h11:1",
-                    "camera_link", # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
+                    "tag36h11:3",
+                    "camera_color_optical_frame", # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
                     rclpy.time.Time())
-                self.orientation = Quaternion(x=t.transform.rotation.x, y=t.transform.rotation.y, z=t.transform.rotation.z, w=t.transform.rotation.w)
-                self.state = State.ADD_BOX
+                
+
+
+                pose_info = f"Position: {t.transform.translation.x, t.transform.translation.y, t.transform.translation.z}"
+
+
+
+                self.get_logger().info("\n" + pose_info + "\n")
+
 
             except TransformException as ex:
                 self.get_logger().info(
-                            f'Could not transform {"tag36h11:1"} to {"camera_link"}: {ex}', once=True)
+                            f'Could not transform {"tag36h11:3"} to {"camera_color_optical_frame"}: {ex}', once=True)
                 return
                     
-            pose_info = f"Position: {t.transform.translation.x, t.transform.translation.y, 0.1*(t.transform.translation.z)}\n"\
-                        f"Orientation: {t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w}"
+            
+            
 
-            self.get_logger().info("\n" + pose_info + "\n")
 
-            self.whiteboard_distance = -0.1 * t.transform.translation.z
 
-        elif self.state == State.ADD_BOX:
-            if self.whiteboard_distance:
-
-                name = "whiteboard"
-                pose = Pose()
-                pose.position.x = 0.0
-                pose.position.y = self.whiteboard_distance
-                pose.position.z = 0.5
-                pose.orientation = self.orientation
-                size = [0.5, 1.0, 0.05]
-                shape = SolidPrimitive(type=SolidPrimitive.BOX, dimensions=size)
-                self.robot.add_box(name=name, pose=pose, shape=shape)
-
-            self.state = State.LOOK_UP_TRANSFORM
 #########################################################################################################################
 
-
-    def broadcast_static_transform(self):
-        # Treating panda_link0 as the world/base_link frame
-        panda_camera_tf = TransformStamped()
-        panda_camera_tf.header.stamp = self.get_clock().now().to_msg()
-        panda_camera_tf.header.frame_id = "panda_link0"
-        panda_camera_tf.child_frame_id = "camera_link"
-
-        panda_camera_tf.transform.translation.x = 0.0
-        panda_camera_tf.transform.translation.y = 0.0
-        panda_camera_tf.transform.translation.z = 0.0
-
-        panda_camera_tf.transform.rotation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
-
-        self.tf_static_broadcaster.sendTransform(panda_camera_tf)
-        self.state = State.LOOK_UP_TRANSFORM
 
         
 def get_apriltags_entry(args=None):
