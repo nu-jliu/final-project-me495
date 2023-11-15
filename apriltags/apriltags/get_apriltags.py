fa14e6c9fa43729467b5cc4ccc0635471a712fb4
Allen Liu <JingkunLiu2025@u.northwestern.edu>
2023-11-15 00:31:53 -0600

    apriltag on franka world

--- apriltags/apriltags/get_apriltags.py
+++ apriltags/apriltags/get_apriltags.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+
 # from tf2_ros.transform_listener import TransformListener
 from tf2_ros import TransformBroadcaster, TransformException
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
@@ -19,13 +20,14 @@ class State(Enum):
 
 class GetAprilTags(Node):
     """Gets April Tag information"""
+
     def __init__(self):
         super().__init__("get_apriltags")
-        
+
         self.state = State.LOOK_UP_TRANSFORM
 
-        self.timer = self.create_timer(1.0/100.0, self.timer_callback)
-        
+        self.timer = self.create_timer(1.0 / 100.0, self.timer_callback)
+
         # Initialize transform information
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
@@ -33,7 +35,7 @@ class GetAprilTags(Node):
 
         # Make static transform between camera_color_optical frame and panda_hand
         self.tf_static_broadcaster = StaticTransformBroadcaster(self)
-        
+
         # self.broadcast_static_transform()
         # Camera in the frame of the hand
         hand_camera_tf = TransformStamped()
@@ -41,41 +43,51 @@ class GetAprilTags(Node):
         hand_camera_tf.header.frame_id = "panda_hand"
         hand_camera_tf.child_frame_id = "camera_link"
 
-        hand_camera_tf.transform.translation.x = 0.50
-        hand_camera_tf.transform.translation.y = 0.0
-        hand_camera_tf.transform.translation.z = 0.65
+        hand_camera_tf.transform.translation.x = 50e-3
+        hand_camera_tf.transform.translation.y = 15e-3
+        hand_camera_tf.transform.translation.z = 65e-3
 
-        hand_camera_tf.transform.rotation.x = Quaternion(x=0.0, y=0.0, z=0.7071068, w=0.7071068)
+        hand_camera_tf.transform.rotation = Quaternion(
+            x=0.8320387, y=-0.0052696, z=0.5546925, w=-0.0000832
+        )
 
         self.tf_static_broadcaster.sendTransform(hand_camera_tf)
 
-# 3 is top left, 4 is bottom left, 1 is bottom right
-#########################################################################################################################
+    # 3 is top left, 4 is bottom left, 1 is bottom right
+    #########################################################################################################################
     def timer_callback(self):
-
         if self.state == State.LOOK_UP_TRANSFORM:
-
             try:
                 t = self.tf_buffer.lookup_transform(
                     "tag36h11:3",
-                    "camera_color_optical_frame", # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
-                    rclpy.time.Time())
-                
-                position_3 = (t.transform.translation.x, t.transform.translation.y, t.transform.translation.z)
-                orientation_3 = Quaternion(t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w)
+                    "camera_color_optical_frame",  # /tf publishes camera_color_optical_frame, not camera link. But camera_link is root. Unsure which to use.
+                    rclpy.time.Time(),
+                )
+
+                position_3 = (
+                    t.transform.translation.x,
+                    t.transform.translation.y,
+                    t.transform.translation.z,
+                )
+                orientation_3 = Quaternion(
+                    x=t.transform.rotation.x,
+                    y=t.transform.rotation.y,
+                    z=t.transform.rotation.z,
+                    w=t.transform.rotation.w,
+                )
 
                 # posi_info = f"Position: {t.transform.translation.x, t.transform.translation.y, t.transform.translation.z}"
 
                 self.get_logger().info("\n" + f"{position_3[2]}" + "\n")
 
-
             except TransformException as ex:
                 self.get_logger().info(
-                            f'Could not transform {"tag36h11:3"} to {"camera_color_optical_frame"}: {ex}', once=True)
+                    f'Could not transform {"tag36h11:3"} to {"camera_color_optical_frame"}: {ex}',
+                    once=True,
+                )
                 return
-                    
-            
-            
+
+
 #########################################################################################################################
 
 # def broadcast_static_transform(self):
@@ -95,7 +107,6 @@ class GetAprilTags(Node):
 #     self.tf_static_broadcaster.sendTransform(hand_camera_tf)
 
 
-    
 def get_apriltags_entry(args=None):
     rclpy.init(args=args)
     node = GetAprilTags()
