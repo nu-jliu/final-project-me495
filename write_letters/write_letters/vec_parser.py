37d847bcca1cf2bde8d5f3ddb67c9d44665a6644
Allen Liu <JingkunLiu2025@u.northwestern.edu>
2023-11-15 21:14:34 -0600

    add subscription

--- write_letters/write_letters/vec_parser.py
+++ write_letters/write_letters/vec_parser.py
@@ -29,13 +29,16 @@ from rclpy.callback_groups import ReentrantCallbackGroup
 from geometry_msgs.msg import Point
 
 # services
-from polyglotbot_interfaces.srv import Path, Write
+from polyglotbot_interfaces.srv import Path, Write, AprilCoords
 
 
 class VecParser(Node):
     def __init__(self):
         super().__init__("vec_parser")
         self.cb_group = ReentrantCallbackGroup()
+        self.april_1: Point = None
+        self.april_2: Point = None
+        self.april_3: Point = None
 
         self.client_points = self.create_client(
             Path, "load_path", callback_group=self.cb_group
@@ -48,9 +51,28 @@ class VecParser(Node):
             callback_group=self.cb_group,
         )
 
+        self.sub_april_coords = self.create_subscription(
+            AprilCoords,
+            "april_tag_coords",
+            callback=self.sub_april_coords_callback,
+            qos_profile=10,
+        )
+
+        self.sub_april_coords = self.create_subscription(
+            AprilCoords,
+            "april_tag_coords",
+            callback=self.sub_april_coords_callback,
+            qos_profile=10,
+        )
+
         while not self.client_points.wait_for_service(timeout_sec=2.0):
-            # raise RuntimeError("Service 'load_path' not available")
-            pass
+            raise RuntimeError("Service 'load_path' not available")
+
+        while not self.count_publishers("april_tags_coords") > 1:
+            self.get_logger().info("Not receiving tag coords waiting again ...")
+
+        while not self.count_publishers("april_tags_coords") > 1:
+            self.get_logger().info("Not receiving tag coords waiting again ...")
 
         # TODO: Make it parameter or get the value from april tags
         self.offset = 0.4
@@ -65,6 +87,13 @@ class VecParser(Node):
         self.z_max = 0.8
 
         # self.__send_points()
+
+    def sub_april_coords_callback(self, msg: AprilCoords):
+        self.april_1 = msg.p1
+        self.april_2 = msg.p2
+        self.april_3 = msg.p3
+
+        self.get_logger().info(f"{self.april_1}, {self.april_2}, {self.april_3}")
 
     def srv_write_callback(self, request, response):
         """
