f22c2b27113373eaccd6ab8eed8854c6bbfe75e0
kassidyshedd <kass92800@gmail.com>
2023-12-02 22:28:31 -0600

    Fixed characters adding lines. Add docstrings, pass colcon tests

--- string2waypoints/string2waypoints/string2waypoint_node.py
+++ string2waypoints/string2waypoints/string2waypoint_node.py
@@ -5,17 +5,45 @@ from matplotlib.textpath import TextPath
 from polyglotbot_interfaces.srv import StringToWaypoint
 import numpy as np
 from geometry_msgs.msg import Point
-import os
 from ament_index_python.packages import get_package_share_directory
 
+
 class CreateWaypoint(Node):
+    """
+    A ROS2 Node for creating waypoints from a given string.
+
+    Attributtes:
+    ------------
+        - waypoint_srv: A ROS2 service for converting a string to a list of waypoints.
+    """
+
     def __init__(self):
+        """
+        Construct the CreateWaypoint class.
+
+        Initializes the 'create_waypoint' node and creates a service for
+        converting strings to waypoints.
+        """
         super().__init__("create_waypoint")
 
-        #Service
-        self.waypoint_srv = self.create_service(StringToWaypoint, 'string2waypoint', self.string2waypoint_callback)
+        # Service
+        self.waypoint_srv = self.create_service(StringToWaypoint, 'string2waypoint',
+                                                self.string2waypoint_callback)
 
     def string2waypoint_callback(self, request, response):
+        """
+        Call back method for the string to waypoint service.
+
+        Arguements
+        ----------colcon test --packages-select string2waypoints --event-handlers console_cohesion+
+
+            - request: The service request containing the string and the language of the string.
+
+        Returns
+        -------
+            - response: The service response containing the waypoints.
+
+        """
         self.string = request.text
         self.language = request.language
         self.waypoint = self.str2waypoint(self.string, self.language)
@@ -24,6 +52,18 @@ class CreateWaypoint(Node):
         return response
 
     def create_waypoints_message(self, waypoints):
+        """
+        Create a list of waypoints into the proper ROS2 message format.
+
+        Arguments
+        ---------
+            - waypoints: List of waypoints.
+
+        Returns
+        -------
+            - point_messages: List of point messages.
+
+        """
         point_messages = []
         for i in waypoints:
             point_messages.append(Point(x=i[0], y=i[1], z=i[2]))
@@ -31,11 +71,23 @@ class CreateWaypoint(Node):
         return point_messages
 
     def str2waypoint(self, string, language):
-        #Prepare for different languages
-        #Need to upload to main package and create package share paths
+        """
+        Convert a string to a list of waypoints based on the specified language.
+
+        Arguments:
+        ----------
+            - string: Input string
+            - language: Language code
+
+        Returns
+        -------
+            - test: A list of waypoints in the form [x, y, z]
+
+        """
+        # Prepare for different languages
+        # Need to upload to main package and create package share paths
         package_name = "string2waypoints"
         package_share_directory = get_package_share_directory(package_name)
-
 
         if language == 'zh-cn':
             fontPath = package_share_directory + "/NotoSansSC-VariableFont_wght.ttf"
@@ -50,20 +102,22 @@ class CreateWaypoint(Node):
         else:
             fontPath = package_share_directory + "/Roboto-Thin.ttf"
 
-        #Create FontProperties object with "custom" google font
+        # Create FontProperties object with "custom" google font
         customFont = FontProperties(fname=fontPath)
+
+        order_of_char = []
 
         startX, startY = 0, 0
 
         for char in string:
             if char.isspace():
                 continue
-            #Path of current character
+            # Path of current character
             charPath = TextPath((startX, startY), char, size=1., prop=customFont)
-            #Get vertices of path
+            # Get vertices of path
             charVerts = charPath.vertices
-            #Remove unnecessary points
-            charVerts = charVerts[~(charVerts[:,0]%1<0.01)]
+            # Remove unnecessary points
+            charVerts = charVerts[~(charVerts[:, 0] % 1 < 0.01)]
             # print(charVerts.shape)
 
             duplicate_entry = []
@@ -76,13 +130,22 @@ class CreateWaypoint(Node):
             # Find indices of duplicate entries
             indices_of_duplicates = []
             for duplicate in duplicate_entry:
-                indices = [index for index, cv in enumerate(charVerts) if np.array_equal(cv, duplicate)]
+                indices = [index for index, cv in enumerate(charVerts)
+                           if np.array_equal(cv, duplicate)]
                 indices_of_duplicates.append(indices)
 
             ind = []
             for i, indices in enumerate(indices_of_duplicates):
                 ind += [indices[1]+1]
+                print(ind)
 
+            order_of_char.append((char, ind))
+        # print("order of chars before sorting:", order_of_char)
+
+        order_of_char = [(char, sorted(ind)) for char, ind in order_of_char]
+        # print("order of char after sorting:", order_of_char)
+
+        for char, ind in order_of_char:
             a = np.split(charVerts, ind)
             a = [arr for arr in a if arr.size > 0]
             a = [np.column_stack((arr, np.full(arr.shape[0], 0))) for arr in a]
@@ -102,7 +165,6 @@ class CreateWaypoint(Node):
                         xyz = [x, y, z]
                         test.append(xyz)
 
-
                     x = a[i][j][0]
                     y = a[i][j][1]
                     z = a[i][j][2]
@@ -110,13 +172,12 @@ class CreateWaypoint(Node):
                     test.append(xyz)
 
                     if i != (len(a) - 1) and j == (len(a[i]) - 1):
-                        x = a[i][j][0]
                         y = a[i][j][1]
                         z = 1.0
                         xyz = [x, y, z]
                         test.append(xyz)
             return test
-           
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -124,29 +185,28 @@ def main(args=None):
     rclpy.spin(node)
     rclpy.shutdown()
 
-
-    # 'af': 'afrikaans' 
+    # 'af': 'afrikaans'
     # 'sq': 'albanian'
-    # 'be': 'belarusian'  
-    # 'bg': 'bulgarian' 
-    # 'ca': 'catalan'  
+    # 'be': 'belarusian'
+    # 'bg': 'bulgarian'
+    # 'ca': 'catalan'
     # 'hr': 'croatian'
-    # 'cs': 'czech' 
-    # 'da': 'danish'  
-    # 'nl': 'dutch'  
+    # 'cs': 'czech'
+    # 'da': 'danish'
+    # 'nl': 'dutch'
     # 'en': 'english'
-    # 'eo': 'esperanto' 
-    # 'et': 'estonian'    
-    # 'tl': 'filipino'  
-    # 'fi': 'finnish'   
-    # 'fr': 'french'     
+    # 'eo': 'esperanto'
+    # 'et': 'estonian'
+    # 'tl': 'filipino'
+    # 'fi': 'finnish'
+    # 'fr': 'french'
     # 'gl': 'galician'
-    # 'de': 'german' 
-    # 'el': 'greek'   
-    # 'hu': 'hungarian'  
+    # 'de': 'german'
+    # 'el': 'greek'
+    # 'hu': 'hungarian'
     # 'is': 'icelandic'
     # 'id': 'indonesian'
-    # 'ga': 'irish' 
+    # 'ga': 'irish'
     # 'it': 'italian'
     # 'la': 'latin'
     # 'lv': 'latvian'
@@ -156,32 +216,32 @@ def main(args=None):
     # 'mt': 'maltese'
     # 'no': 'norwegian'
     # 'pl': 'polish'
-    #'pt': 'portuguese'
+    # 'pt': 'portuguese'
     # 'ro': 'romanian'
     # 'ru': 'russian'
     # 'sr': 'serbian'
     # 'sk': 'slovak'
     # 'sl': 'slovenian'
     # 'es': 'spanish'
-    # 'sw': 'swahili' 
+    # 'sw': 'swahili'
     # 'sv': 'swedish'
     # 'tr': 'turkish'
     # 'uk': 'ukrainian'
-    # 'vi': 'vietnamese' 
-    # 'cy': 'welsh' 
+    # 'vi': 'vietnamese'
+    # 'cy': 'welsh'
 
     # 'iw': 'hebrew'
-    # 'yi': 'yiddish' 
+    # 'yi': 'yiddish'
 
-    # 'zh-CN': 'chinese_simplified'         
-    # 'zh-TW': 'chinese_traditional' 
+    # 'zh-CN': 'chinese_simplified'
+    # 'zh-TW': 'chinese_traditional'
 
-    # 'ja': 'japanese'  
-                    
-    # 'ko': 'korean'  
+    # 'ja': 'japanese'
+
+    # 'ko': 'korean'
 
     # Needs work
     # 'ar': 'arabic'
-    # 'hi': 'hindi'                                                                           
+    # 'hi': 'hindi'
     # 'fa': 'persian'
     # 'th': 'thai'
