2b75eba0690110d8f2e3f7c5434244b5faa7080c
blackm159 <meganblack2027@u.northwestern.edu>
2023-12-03 23:09:19 -0600

    Working code for listening to spoken words added to polyglotbot state machine

--- polyglotbot/polyglotbot/polyglotbot.py
+++ polyglotbot/polyglotbot/polyglotbot.py
@@ -70,6 +70,7 @@ class State(Enum):
 
     MARKER = auto(),  # Picks up the marker
     WAITING = auto(),  # Waiting to receive go-ahead to translate
+    LISTENING = auto(),  # Listening to speech
     PERSON = auto(),  # Detected person in frame
     SCANNING = auto(),  # Scans the latest frame for words
     TRANSLATING = auto(),  # Translates the scanned words
@@ -108,8 +109,14 @@ class Polyglotbot(Node):
         while self.count_publishers("writer_state") < 1:
             self.get_logger().info("waiting for writer_state publisher", once=True)
 
+        self.listen_sub = self.create_subscription(String, "listen", self.listen_callback, 10)
+        while self.count_publishers("listen") < 1:
+            self.get_logger().info("waiting for listen publisher", once=True)
+
         # Services
         self.srv_start_translating = self.create_service(Empty, "start_translating", self.start_translating_callback, callback_group=self.cbgroup,)
+
+        self.toggle_mode = self.create_service(TranslateString, "toggle_mode", self.toggle_mode_callback, callback_group=self.cbgroup,)
 
         # Clients
         self.get_characters_client = self.create_client(GetCharacters, "get_characters", callback_group=self.cbgroup)
@@ -124,9 +131,9 @@ class Polyglotbot(Node):
         while not self.cli_translate_string.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("input_msg service not available, waiting again...")
 
-        # self.speak_client = self.create_client(SpeakText, "speak", callback_group=self.cbgroup)
-        # while not self.speak_client.wait_for_service(timeout_sec=2.0):
-        #     self.get_logger().info("speak service not available, waiting again ...")
+        self.speak_client = self.create_client(SpeakText, "speak", callback_group=self.cbgroup)
+        while not self.speak_client.wait_for_service(timeout_sec=2.0):
+            self.get_logger().info("speak service not available, waiting again ...")
 
         self.waypoints_client = self.create_client(StringToWaypoint, "string2waypoint", callback_group=self.cbgroup)
         while not self.waypoints_client.wait_for_service(timeout_sec=1.0):
@@ -152,6 +159,10 @@ class Polyglotbot(Node):
         while not self.write_state_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("change_writer_state service not available, waiting again ...")
 
+        self.listen_client = self.create_client(TranslateString, "record")
+        while not self.listen_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("listen service not available, waiting again ...")
+
         # Timer
         self.tmr = self.create_timer(self.dt, self.timer_callback)
 
@@ -163,6 +174,11 @@ class Polyglotbot(Node):
         self.target_language = None
         self.translated_string = None
 
+        self.listen_lang = "en"
+
+        # False means reading whiteboard, True means listening to speech
+        self.listen_flag = False
+
         self.char_num = 0
         self.waypoints = []
 
@@ -170,36 +186,6 @@ class Polyglotbot(Node):
         self.april_cords_received = False
         self.writer_state = ""
         self.m: Marker = None
-
-    def add_person_detection(self, num_person):
-        self.m = Marker()
-        self.m.header.frame_id = "panda_link0"
-        self.m.header.stamp = self.get_clock().now().to_msg()
-        self.m.id = 15
-        self.m.type = Marker.TEXT_VIEW_FACING
-        self.m.action = Marker.ADD
-        self.m.scale.x = 1.0
-        self.m.scale.y = 1.0
-        self.m.scale.z = 1.0
-        self.m.text = f"Number of person: {num_person}"
-        self.m.lifetime.sec = 1
-        self.m.pose.position.x = 0.0
-        self.m.pose.position.y = 0.4
-        self.m.pose.position.z = 0.7
-        self.m.pose.orientation.x = 0.0
-        self.m.pose.orientation.y = 0.0
-        self.m.pose.orientation.z = 0.7071068
-        self.m.pose.orientation.w = 0.7071068
-        self.m.color.r = 1.0
-        self.m.color.g = 1.0
-        self.m.color.b = 1.0
-        self.m.color.a = 0.8
-        self.markers.publish(self.m)
-
-    def remove_person_detection(self):
-        if self.m:
-            self.m.action = Marker.DELETE
-            self.markers.publish(self.m)
 
     def timer_callback(self):
         """Control the Franka."""
@@ -239,8 +225,17 @@ class Polyglotbot(Node):
         elif self.state == State.WAITING:
             # Wait for person to be detected
             self.get_logger().info("Waiting", once=True)
-            if self.num_people > 0.3:
+            if self.listen_flag == False and self.num_people > 0.3:
                 self.state = State.PERSON
+            # elif self.listen_flag == True:
+            #     future_listen = self.listen_client.call_async(TranslateString.Request(input=self.listen_lang))
+            #     future_listen.add_done_callback(self.future_listen_callback)
+                # self.state = State.LISTENING
+
+        elif self.state == State.LISTENING:
+            self.get_logger().info("Listening", once=True)
+            # if self.source_string != None:
+            #     self.state = State.TRANSLATING
 
         elif self.state == State.PERSON:
             # Wait for person to leave
@@ -262,6 +257,8 @@ class Polyglotbot(Node):
         elif self.state == State.TRANSLATING:
             # Translate the scanned words
             self.get_logger().info("Translating")
+            self.get_logger().info("Target language: %s" % self.target_language)
+            self.get_logger().info("Source string: %s" % self.source_string)
             req = TranslateString.Request()
             req.input = self.target_language
             future_target_language = self.cli_target_language.call_async(req)
@@ -314,6 +311,7 @@ class Polyglotbot(Node):
             self.char_num = 0
             self.waypoints = []
             self.state = State.WAITING
+            self.listen_flag = False
 
         elif self.state == State.PROCESSING:
             if self.writer_state == "State.DONEWRITING":
@@ -321,6 +319,36 @@ class Polyglotbot(Node):
                 future_done_writing = self.write_state_client.call_async(Empty.Request())
                 future_done_writing.add_done_callback(self.future_done_writing_callback)
                 # self.state = State.COMPLETE
+
+    def add_person_detection(self, num_person):
+        self.m = Marker()
+        self.m.header.frame_id = "panda_link0"
+        self.m.header.stamp = self.get_clock().now().to_msg()
+        self.m.id = 15
+        self.m.type = Marker.TEXT_VIEW_FACING
+        self.m.action = Marker.ADD
+        self.m.scale.x = 1.0
+        self.m.scale.y = 1.0
+        self.m.scale.z = 1.0
+        self.m.text = f"Number of person: {num_person}"
+        self.m.lifetime.sec = 1
+        self.m.pose.position.x = 0.0
+        self.m.pose.position.y = 0.4
+        self.m.pose.position.z = 0.7
+        self.m.pose.orientation.x = 0.0
+        self.m.pose.orientation.y = 0.0
+        self.m.pose.orientation.z = 0.7071068
+        self.m.pose.orientation.w = 0.7071068
+        self.m.color.r = 1.0
+        self.m.color.g = 1.0
+        self.m.color.b = 1.0
+        self.m.color.a = 0.8
+        self.markers.publish(self.m)
+
+    def remove_person_detection(self):
+        if self.m:
+            self.m.action = Marker.DELETE
+            self.markers.publish(self.m)
 
     # Subscriber Callbacks
     # #############################################################################################################
@@ -350,11 +378,18 @@ class Polyglotbot(Node):
         """Callback for when a person is detected in the frame."""
         self.num_people = msg.data
 
-        if self.state == State.WAITING or self.state == State.PERSON:
+        if self.listen_flag == False and (self.state == State.WAITING or self.state == State.PERSON):
         # if self.state == State.PERSON:
             # self.remove_person_detection()
             # self.add_person_detection(self.num_people)
             self.get_logger().info(f"Number of people detected: {self.num_people}")
+
+    def listen_callback(self, msg):
+        """Callback for when the listen service returns a string."""
+        if self.listen_flag == True:
+            self.source_string = msg.data
+            self.get_logger().info(f"source_string: {self.source_string}")
+            self.state = State.TRANSLATING
 
     # Service Callbacks
     # #############################################################################################################
@@ -362,6 +397,17 @@ class Polyglotbot(Node):
     def start_translating_callback(self, request, response):
         self.get_logger().info("Polyglotbot beginning translation...")
         self.state = State.SCANNING
+        return response
+    
+    def toggle_mode_callback(self, request, response):
+        self.listen_flag = True
+        self.get_logger().info(f"listen_flag: {self.listen_flag}")
+        self.listen_lang = request.input
+
+        future_listen = self.listen_client.call_async(TranslateString.Request(input=self.listen_lang))
+        future_listen.add_done_callback(self.future_listen_callback)
+
+        response = TranslateString.Response()
         return response
 
     # Future Callbacks
@@ -406,8 +452,8 @@ class Polyglotbot(Node):
     def future_translated_string_callback(self, future_translated_string):
         self.translated_string = future_translated_string.result().output
         self.get_logger().info("Translated string: %s" % self.translated_string)
-        # self.state = State.SPEAKING
-        self.state = State.CREATE_WAYPOINTS
+        self.state = State.SPEAKING
+        # self.state = State.CREATE_WAYPOINTS
 
     def future_speak_callback(self, future_speak):
         result = future_speak.result()
@@ -433,6 +479,11 @@ class Polyglotbot(Node):
     def future_grab_pen_callback(self, future_grab_pen):
         self.get_logger().info(f"{future_grab_pen.result()}")
         # self.state = State.WAITING
+
+    def future_listen_callback(self, future_listen):
+        self.target_language = "en"
+        # self.source_string = future_listen.result()
+        self.state = State.LISTENING
 
 
 def entry_point(args=None):
--- speech/speech/listen_speech.py
+++ speech/speech/listen_speech.py
@@ -2,15 +2,21 @@ import rclpy
 from rclpy.node import Node
 from enum import Enum, auto
 import speech_recognition as sr
+import sounddevice
+from polyglotbot_interfaces.srv import TranslateString
+from std_msgs.msg import String
+
 
 """
 Github: https://github.com/Uberi/speech_recognition/tree/master
 Documentation: https://pypi.org/project/SpeechRecognition/
 """
 
+
 class State(Enum):
     WAITING = auto()
     LISTENING = auto()
+    RECOGNIZING = auto()
 
 
 class ListenSpeech(Node):
@@ -20,33 +26,79 @@ class ListenSpeech(Node):
         super().__init__("listen_speech")
         self.state = State.WAITING
 
-        self.timer = self.create_timer(1.0 / 100.0, self.timer_callback)
+        self.timer = self.create_timer(1.0/100.0, self.timer_callback)
+
+        self.srv_listen = self.create_service(TranslateString, "record", self.record_callback)
+
+        self.listen_pub = self.create_publisher(String, "listen", 10)
 
         # Create a recognizer instance...
         self.recognizer = sr.Recognizer()
+        self.audio = None
+        self.spoken_language = None
+        self.text = None
 
-    
+        self.index = 0
+
+        for index, name in enumerate(sr.Microphone().list_microphone_names()):
+            if "USB Audio" in name:
+                self.index = index
+                self.get_logger().info(f'index: {self.index}')
+
+    def record_callback(self, request, response):
+        self.spoken_language = request.input
+        response.output = self.spoken_language
+        self.state = State.LISTENING
+
+        return response
+
     def timer_callback(self):
+        # for index, name in enumerate(sr.Microphone().list_microphone_names()):
+        #     print(f"Microphone with index {index}: {name}")
+        # self.get_logger().info(f'index: {self.index}')
+
+        self.get_logger().info("Running node...", once=True)
+        # self.state = State.LISTENING
+
+        if self.state == State.WAITING:
+            self.get_logger().info("Waiting for record source language...", once=True)
+
+            self.audio = None
+            self.spoken_language = None
+            self.text = None
+            # pass
 
         if self.state == State.LISTENING:
             # Currently using default microphone (from computer) as audio source
-            with sr.Microphone() as source:
-                print("Say something...")
+            # with sr.Microphone() as source:
+            with sr.Microphone(device_index=self.index) as source:
+                self.get_logger().info("Say something...")
                 # Adjust for ambient noise (if necessary)
                 self.recognizer.adjust_for_ambient_noise(source)
                 # Listen for speech (by default, it listens until it detects a pause)
-                audio = self.recognizer.listen(source)
+                self.audio = self.recognizer.listen(source)
 
+            self.state = State.RECOGNIZING
+
+        elif self.state == State.RECOGNIZING:
+
+            # Turn the recorded language into a string
             try:
-                lang = 'en'
-                text = self.recognizer.recognize_google(audio, language=lang)
-                print("You said:", text)
+                self.get_logger().info("Recognizing...", once=True)
+                self.text = self.recognizer.recognize_google(self.audio, language=self.spoken_language)
+                self.get_logger().info(f"You said: {self.text}")
+                self.listen_pub.publish(String(data=self.text))
             except sr.UnknownValueError:
-                print("Sorry, could not understand audio")
+                self.get_logger().info("Sorry, could not understand audio")
             except sr.RequestError as e:
-                print("Error:", str(e))
-            
+                self.get_logger().info("Error:", str(e))
+
             self.state = State.WAITING
+            self.get_logger().info("Waiting for record source language...")
+
+# I think next I need to publish the speech onto a topic
+# In addition, need to figure out how to get name of the language that is being spoken in
+
 
 def listen_speech_entry(args=None):
     rclpy.init(args=args)
